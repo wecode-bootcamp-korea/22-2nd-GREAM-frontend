21c8b396fc1328d12745085548a7719b54e98e47
silviaoh <silvia.jisu.oh@gmail.com>
2021-07-20 12:06:12 +0900

    Modify: setFlex 위치 수정

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import ReactDOM from 'react-dom';
 import Routes from './Routes';
 import GlobalStyle from './styles/GlobalStyle.js';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './styles/theme';
+import theme from './styles/theme';
+
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <Routes />
