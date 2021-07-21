7c8f572aa93b7c8e6e5789d2036eb37c297a4afe
silviaoh <silvia.jisu.oh@gmail.com>
2021-07-21 20:03:17 +0900

    Rebase: Modify/fontawesome commits - Modify: fontawesome 라이브러리 삭제 & fontawesome, font 관련 코드 수정 - Delete: Detail 안의 코드 삭제

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,8 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
 
-@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
@@ -21,7 +19,6 @@ time, mark, audio, video {
 	padding: 0;
 	border: 0;
 	font-size: 100%;
-	font: inherit;
 	vertical-align: baseline;
 }
 /* HTML5 display-role reset for older browsers */
@@ -29,9 +26,10 @@ article, aside, details, figcaption, figure,
 footer, header, hgroup, menu, nav, section {
 	display: block;
 }
+
 * {
   box-sizing: border-box;
-  font-family: "Roboto", sans-serif !important;
+  font-family: 'Roboto', sans-serif;
 }
 
 body {
