28f264a32f27cc31ebcf70df58696b689ef68d35
Junghoon-P <qkr0454@naver.com>
2021-07-28 11:07:35 +0900

    Add: side 필터링링
    - sideFitler 기능 구현(선택, 중복선택)

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-const BASE_URL = '10.58.7.247';
-export const PRODUCTS_API = `http://${BASE_URL}:8000/products`;
+const BASE_URL = 'http://18.222.211.21:8000';
+export const PRODUCTS_API = `${BASE_URL}/products`;
+export const PRODUCTS_CATEGORY_API = `${BASE_URL}/products/category`;
--- src/pages/Shop/SideFilterList/DropDownFilter/DropDownFilter.jsx
+++ src/pages/Shop/SideFilterList/DropDownFilter/DropDownFilter.jsx
@@ -2,13 +2,29 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Checkbox from '@material-ui/core/Checkbox';
 
-const DropDownFilter = props => {
+const DropDownFilter = ({
+  name,
+  getCategory,
+  id,
+  categoryName,
+  removeSelected,
+}) => {
+  const categoryFilterHandler = e => {
+    console.log(`e`, e);
+    const checked = e.target.checked;
+    if (checked) getCategory(id, categoryName);
+    else if (!checked) removeSelected(id, categoryName);
+  };
+
   return (
     <Drop>
       <div>
-        <Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
+        <Checkbox
+          onClick={e => categoryFilterHandler(e)}
+          inputProps={{ 'aria-label': 'uncontrolled-checkbox' }}
+        />
       </div>
-      <span>{props.name}</span>
+      <span>{name}</span>
     </Drop>
   );
 };
@@ -22,6 +38,7 @@ const Drop = styled.div`
   div {
     margin-right: 4px;
     text-align: center;
+    z-index: 90;
     i {
       font-size: 10px;
       color: #ffffff;
